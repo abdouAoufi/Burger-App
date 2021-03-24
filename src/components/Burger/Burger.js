def65dfd5e84503d36f22b311d51ee4cff9fdd13
Abderahmane Aoufi <abdouou7@gmail.com>
2021-03-24 12:22:15 +0100

    rebuilding the project

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,20 +5,6 @@ import cssClasses from "./burger.css";
 
 class Burger extends React.Component {
   render() {
-<<<<<<< HEAD
-    this.transformIngredient = Object.keys(this.props.ingredients)
-      .map((ijKey) => {
-        return [...Array(this.props.ingredients[ijKey])].map((_, i) => (
-          <BurgerIngredient key={i + ijKey} type={ijKey} />
-        ));
-      })
-      .reduce((arr, el) => {
-        return arr.concat(el);
-      }, []);
-    if (this.transformIngredient.length === 0) {
-      this.transformIngredient = <p>Please start adding ingredients !!</p>;
-    }
-=======
      this.transformIngredient = Object.keys(this.props.ingredients)
     .map((ijKey) => {
       return [...Array(this.props.ingredients[ijKey])].map((_, i) => (
@@ -32,7 +18,6 @@ class Burger extends React.Component {
         if(this.transformIngredient.length === 0){
             this.transformIngredient = <p>Please start adding ingredients !!</p>
         }
->>>>>>> 1682d619a639236dbe01a68989ffa1e50dce3dab
     return (
       <div className={cssClasses.Burger}>
         <BurgerIngredient key={"bread-top"} type={"bread-top"} />
@@ -40,13 +25,6 @@ class Burger extends React.Component {
         <BurgerIngredient key={"bread-bottom"} type={"bread-bottom"} />
       </div>
     );
-<<<<<<< HEAD
-  }
-
-  changeIngredient() {
-    console.log("message");
-=======
->>>>>>> 1682d619a639236dbe01a68989ffa1e50dce3dab
   }
 }
 export default Burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,15 +2,9 @@
 import React from "react";
 import Aux from "../../hoc/Aux";
 import Burger from "../../components/Burger/Burger";
-<<<<<<< HEAD
-import BuildControl from "../../components/Burger/BuildControl/BuildControl";
-import withClass from "../../components/hoc/WithClass";
-import cssClasses from "./BurgerBuilder.css";
-=======
 import BuildControls from "../../components/Burger/BuildControl/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
->>>>>>> 1682d619a639236dbe01a68989ffa1e50dce3dab
 
 const INGREDIENT_PRICE = {
   salad: 0.5,
@@ -21,135 +15,6 @@ const INGREDIENT_PRICE = {
 class BurgerBuilder extends React.Component {
   state = {
     ingredients: {
-<<<<<<< HEAD
-      salad: 1,
-      bacon: 1,
-      cheese: 1,
-      meat: 1,
-    },
-    ingredientsPrice: {
-      salad: 3,
-      bacon: 5,
-      cheese: 4,
-      meat: 10,
-    },
-  };
-
-  render() {
-    console.log("BurgerBuilder ======> ", this.state);
-    return (
-      <Aux>
-        <BuildControl
-          ingredients={this.state.ingredients}
-          ingredientsPrice={this.state.ingredientsPrice}
-          clickPlus={this.changeIngridientPlus}
-          clickMinus={this.changeIngridientMinus}
-        />
-        <Burger ingredients={this.state.ingredients} />
-      </Aux>
-    );
-  }
-
-  changeIngridientPlus = (element) => {
-    let meat = this.state.ingredients.meat;
-    let cheese = this.state.ingredients.cheese;
-    let bacon = this.state.ingredients.bacon;
-    let salad = this.state.ingredients.salad;
-    let cheesePrice = this.state.ingredientsPrice.cheese;
-    let baconPrice = this.state.ingredientsPrice.bacon;
-    let meatPrice = this.state.ingredientsPrice.meat;
-    let saladPrice = this.state.ingredientsPrice.salad;
-    switch (element) {
-      case "Cheese":
-        cheese = this.state.ingredients.cheese + 1;
-        cheesePrice = cheese * 4;
-        console.log(cheesePrice);
-        break;
-      case "Meat":
-        meat = this.state.ingredients.meat + 1;
-        meatPrice = meat * 10;
-        break;
-      case "Bacon":
-        bacon = this.state.ingredients.bacon + 1;
-        baconPrice = bacon * 5;
-        break;
-      case "Salad":
-        salad = this.state.ingredients.salad + 1;
-        saladPrice = salad * 3;
-        break;
-      default:
-        break;
-    }
-
-    this.setState({
-      ingredients: {
-        cheese,
-        meat,
-        bacon,
-        salad,
-      },
-      ingredientsPrice: {
-        cheese: cheesePrice,
-        meat: meatPrice,
-        bacon: baconPrice,
-        salad: saladPrice,
-      },
-    });
-  };
-
-  changeIngridientMinus = (element) => {
-    let meat = this.state.ingredients.meat;
-    let cheese = this.state.ingredients.cheese;
-    let bacon = this.state.ingredients.bacon;
-    let salad = this.state.ingredients.salad;
-    switch (element) {
-      case "Cheese":
-        if (this.state.ingredients.cheese === 0) {
-          cheese = 0;
-        } else {
-          cheese = this.state.ingredients.cheese - 1;
-        }
-        break;
-
-      case "Meat":
-        if (this.state.ingredients.meat === 0) {
-          meat = 0;
-        } else {
-          meat = this.state.ingredients.meat - 1;
-        }
-        break;
-
-      case "Bacon":
-        if (this.state.ingredients.bacon === 0) {
-          bacon = 0;
-        } else {
-          bacon = this.state.ingredients.bacon - 1;
-        }
-        break;
-      case "Salad":
-        if (this.state.ingredients.salad === 0) {
-          salad = 0;
-        } else {
-          salad = this.state.ingredients.salad - 1;
-        }
-        break;
-      default:
-        break;
-    }
-
-    this.setState({
-      ingredients: {
-        cheese,
-        meat,
-        bacon,
-        salad,
-      },
-    });
-  };
-}
-
-export default withClass(BurgerBuilder, cssClasses.BurgerBuilder);
-=======
       salad: 0,
       bacon: 0,
       cheese: 0,
@@ -242,4 +107,3 @@ export default withClass(BurgerBuilder, cssClasses.BurgerBuilder);
   };
 }
 export default BurgerBuilder;
->>>>>>> 1682d619a639236dbe01a68989ffa1e50dce3dab
