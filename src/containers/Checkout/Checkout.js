ddddc3ef934b9decb1f4d62b411242f76644118d
Abderahmane Aoufi <abdouou7@gmail.com>
2021-04-15 20:05:22 +0100

    contacct to data

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import CheckoutSummary from "../Order/CheckoutSummary/CheckoutSummary";
-
+import ContactData from "./ContactData/ContactData";
 export default class Checkout extends Component {
   state = {
     ingredients: {
+      sum : 0 ,
       salad: 0,
       cheese: 0,
       meat: 0,
@@ -11,14 +12,24 @@ export default class Checkout extends Component {
     },
   };
 
+  checkViablity = (ingredients) => {
+    let toatalPrice = 0 ;
+    Object.values(ingredients).forEach(price => {toatalPrice+=price});
+    return toatalPrice ;
+  }
+
   componentDidMount() {
-    console.log(this.props.location.search);
+    // console.log(this.props);
     const query = new URLSearchParams(this.props.location.search);
     const ingredients = {};
     for (let param of query.entries()) {
       ingredients[param[0]] = +param[1];
     }
-    this.setState({ ingredients: ingredients });
+    this.setState({  ingredients: ingredients });
+    ;
+    if(this.checkViablity(ingredients) < 1){
+      this.props.history.replace("/");
+    }
   }
   checkoutCanceledHandler = () => {
     this.props.history.goBack();
@@ -27,7 +38,9 @@ export default class Checkout extends Component {
   checkoutContinuedHandler = () => {
     this.props.history.replace("/checkout/contact-data"); // because checkout is direct child to the parent
   };
+
   render() {
+ 
     return (
       <div>
         <CheckoutSummary
@@ -35,6 +48,11 @@ export default class Checkout extends Component {
           checkoutCanceled={this.checkoutCanceledHandler}
           checkoutContinued={this.checkoutContinuedHandler}
         />
+        <Route
+          path={this.props.match.url + "/contact-data"}
+          component={ContactData}
+        />
+      
       </div>
     );
   }
